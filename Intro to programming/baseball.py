6d53b20a577b7210951976c8462c40832dc241e6
Jacob Wendel <jacobwendel@Jacobs-MacBook-Air-3.local>
2025-01-23 10:00:46 -0700

    wireframe practice

---
+++ Intro to programming/baseball.py
@@ -0,0 +1,10 @@
+print("please enter the baseball palyer's information")
+name = input("player name: ")
+age = int(input("player age: "))
+batting_avg = float(input("Player's batting average: "))
+
+print("----------------------------")
+print("Player summary")
+print(f"Name: {name.title()}")
+print(f"Age: {age}")
+print(f"Batting average: {batting_avg}")
---
+++ Intro to programming/prove3.py
@@ -0,0 +1,3 @@
+print("Please input the follow prices")
+kids = int(input("Kids meal"))
+adult = int(input("Adult meal"))
