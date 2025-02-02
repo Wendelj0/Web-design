ec5da97ee64a081f188cbfc18b28ac467114abdf
Jacob Wendel <jacobwendel@Jacobs-MacBook-Air-3.local>
2025-02-01 18:40:24 -0700

    we pushin

---
+++ Intro to programming/pizza.py
@@ -0,0 +1,16 @@
+import math
+#problem 1
+a = 3
+b = 5
+c = math.sqrt(a**2 + b**2)
+#problem 2
+print(c)
+a = 5
+b = 12
+c = math.sqrt(a**2 + b**2)
+#problem 3
+print(c)
+a = 8
+b = 17
+c = math.sqrt(a**2 + b**2)
+print(c)
--- Intro to programming/prove3.py
+++ Intro to programming/prove3.py
@@ -1,16 +1,23 @@
-kids_meal = float(input("How much does the kids meal cost: "))
-adult_meal = float(input("How much does the adult meal cost: "))
+print("Welcome to Mcdonalds please enter the following prices")
+kids_meal = float(input("How much does the happy meal for kids cost: "))
+adult_meal = float(input("How much does the adult big mac meal cost: "))
 kids = int(input("How many kids are there: "))
 adult = int(input("how many adults are there: "))
-tax = float(input("What is the sales tax: "))
+tax = float(input("What is the sales tax rate: "))
+
 print()
 print()
+
+tax_rate = float(tax * 0.01)
 subtotal = float(kids_meal * kids + adult_meal * adult)
-total = float(subtotal + tax)
-print(f"Subtotal: {subtotal}")
-print(f"Sales tax: {tax}")
-print(f"Total: {total}")
-print()
+sales_tax = float(subtotal * tax_rate)
+
+print(f"Subtotal: {subtotal:.2f}")
+
+
+print(f"Sales tax: {sales_tax:.2f}")
+ttotal = print(f"Total: {subtotal + sales_tax:.2f}")
+
 pay = float(input("Payment amount: "))
-change = float(pay - total)
-print(f"Change: {change}")
+change = float(pay - ttotal)
+print(f"Change: {change:.2f}")
